4afb15372b7366abf78dbad1bff2d36b3fc1dc92
Sunny Wu <w.sunny0618@gmail.com>
2025-06-22 12:34:07 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// fetch weather
+function fetchWeather(){
+
+    // variable to store value in text field
+    let searchInput = document.getElementById('search').value;
+
+    // constant to store weather data div
+    const weatherDataSection = document.getElementById('weather-data');
+    
+    // make weather data div visible
+    weatherDataSection.style.display = 'block';
+    
+    // constant to store api key
+    const apiKey = '6add99df8cb9224dfd1c54dc1f518136';
+}
